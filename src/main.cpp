b79c32e65d84eaae5b30f222007ada23b80bdcdf
praktikantfes <praktikantfes@gmail.com>
2026-01-20 14:47:17 +0100

    neues Projekt!

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,22 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DHT.h>
+#include <WiFi.h>
+#include <wifi_credentials.h>
+#include <WebServer.h>
 using namespace std;
 
-// LED1 an GPIO25, LED2 an GPIO26 angeschlossen
+WebServer server(80);
+
+/*
+      Variablen
+                    */ 
+
+int lastState = HIGH;
+int page = 0;
+bool toggled = false;
+
 int led1 = 25;
 int led2 = 26;
 int buttonPin = 33;
@@ -13,16 +26,25 @@ int buttonPin = 33;
 int ScreenSda = 35;
 int ScreenScl = 32;
 
-// Zustände der LEDs (an/aus)
+int licht = analogRead(32);
+
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define DHTTYPE DHT11
+#define DHTPIN 27
+DHT dht(DHTPIN,DHTTYPE);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 #define NUMFLAKES 10
 
-int licht = analogRead(32);
+float hum = dht.readHumidity();
+float temp = dht.readTemperature();
+
+/*
+    Funktionen
+                */
 
 void showWelcomePage() {
   display.clearDisplay();
@@ -44,6 +66,7 @@ void Showlightpage() {
   display.setTextSize(1);
   display.println(analogRead(32));
   display.display();
+  delay(250);
 }
 
 void setup() {
@@ -51,48 +74,77 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  dht.begin();
   Serial.begin(9600);
   Serial.println("Programm Gestartet.");
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
     Serial.println("Fehler beim initialisieren des Displays");
     for(;;);
   }
+
+  server.on("/temperature", []() {
+      server.send(200, "text/plain", String(temp));
+  });
+  server.begin();
+
   display.clearDisplay();
   showWelcomePage();
+  delay(5000);
+  Serial.println(WiFi.localIP());
 }
 
-int lastState = HIGH;
+void showtempPage() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.print("Temperature: ");
+  display.print(temp);
+  display.println("°C");
+  display.println("Humidity: ");
+  display.print(hum);
+  display.print("%");
+  display.display();
+}
 
-bool toggled = false;
-
-int page = 0;
-
+void flash_light() {
+      digitalWrite(led1, HIGH);
+      delay(25);
+      digitalWrite(led1 ,LOW);
+      digitalWrite(led2, HIGH);
+      delay(25);
+      digitalWrite(led2, LOW);
+      digitalWrite(led1, HIGH);
+      delay(10);
+      digitalWrite(led1, LOW);
+}
 
 void loop() {
-  // LICHTSENSOR
+  
 
-if (digitalRead(buttonPin) == LOW) {
+  if (digitalRead(buttonPin) == LOW) {
     if (lastState == HIGH) {
-      lastState = LOW;
-      Serial.println("Button pressed");
-      ++page;
-      display.clearDisplay();
-      display.display();
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      delay(100);
-      digitalWrite(led1 ,LOW);
-      digitalWrite(led2, LOW);
+        lastState = LOW;
+        ++page;
+        Serial.println("Button pressed");
+        display.clearDisplay();
+        display.display();
+        flash_light();
+        Serial.println(WiFi.localIP());
+        Serial.println(WiFi.status());
     }
-    // Serial.println(lastState);
+      // Serial.println(lastState);
   } else {
     lastState = HIGH;
     // Serial.println(lastState);
   }
-  if (page >= 2) page = 0;
+  if (page >= 3) page = 0;
   if (page == 0) {
-    showWelcomePage();
+      showWelcomePage();
   } else if (page == 1) {
-    Showlightpage();
+      Showlightpage();
+  } else if (page == 2) {
+    showtempPage();
   }
 }
---
+++ src/wifi_credentials.h
@@ -0,0 +1,2 @@
+const char* WIFI_SSID = "Jermaine's iPhone";
+const char* WIFI_PASSWORD = "etistinkt";
