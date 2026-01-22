8d4d12d971533e67d850096a679bd8cb415cd08a
praktikantfes <praktikantfes@gmail.com>
2026-01-22 10:11:44 +0100

    neues Projekt!

--- src/main.cpp
+++ src/main.cpp
@@ -15,26 +15,29 @@ WebServer server(80);
       Variablen
                     */ 
 
-int lastState = HIGH;
-int currentState = HIGH;
+int lastState1 = HIGH;
+int currentState1 = HIGH;
+int lastState2 = HIGH;
+int currentState2 = HIGH;
 int page = 0;
 bool toggled = false;
 bool ledAn = false;
 
 int led1 = 25;
 int led2 = 26;
-int buttonPin = 33;
+int forwardButton = 33;
+int backwardButton = 32;
 
-int ScreenSda = 35;
-int ScreenScl = 32;
+int ScreenSda = 21;
+int ScreenScl = 22;
 
-int licht = analogRead(32);
+int licht = analogRead(34);
 
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define DHTTYPE DHT11
-#define DHTPIN 27
+#define DHTPIN 35
 DHT dht(DHTPIN,DHTTYPE);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -50,12 +53,13 @@ float temp = dht.readTemperature();
 
 void showWelcomePage() {
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setCursor(50, 20);
+  display.setTextSize(1.7);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  display.println("Wilkommen");
+  display.println("Wilkommen (1)");
   display.setTextSize(0.8);
-  display.println("Geräte-IP: ");
+  display.println("Geraete-IP: ");
   display.println(WiFi.localIP());
   display.println("Wlan Status: ");
   if (WiFi.status() == WL_CONNECTED) {
@@ -69,15 +73,15 @@ void showWelcomePage() {
 
 void Showlightpage() {
   display.clearDisplay();
-  display.setTextSize(0.7);
+  display.setTextSize(1.7);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  display.println("Helligkeit");
-  display.setTextSize(2);
-  display.print(map(analogRead(32), 0, 4095, 0, 100));
+  display.println("Helligkeit (2)");
+  display.setTextSize(1.3);
+  display.print(map(analogRead(34), 0, 4095, 0, 100));
   display.println("%");
   display.setTextSize(1);
-  display.print(analogRead(33));
+  display.print(analogRead(34));
   display.print(" (0-4095)");
   display.display();
   delay(250);
@@ -86,7 +90,8 @@ void Showlightpage() {
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(forwardButton, INPUT_PULLUP);
+  pinMode(backwardButton, INPUT_PULLUP);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   dht.begin();
   Serial.begin(9600);
@@ -108,68 +113,73 @@ void setup() {
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("Verbunden");
   }
-  
 }
 
-
-/*
 void showtempPage() {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  display.print("Temperature: ");
+  display.print("Temperatur: ");
   display.print(temp);
   display.println("°C");
-  display.println("Humidity: ");
+  display.println("Luftfeuchtigkeit: ");
   display.print(hum);
   display.print("%");
   display.display();
 }
 
-
 void flash_light() {
-      digitalWrite(led1, HIGH);
-      delay(25);
-      digitalWrite(led1 ,LOW);
-      digitalWrite(led2, HIGH);
-      delay(25);
-      digitalWrite(led2, LOW);
-      digitalWrite(led1, HIGH);
-      delay(10);
-      digitalWrite(led1, LOW);
-}
-*/
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  delay(25);
+  digitalWrite(led1 ,LOW);
+  digitalWrite(led2, LOW);
 
+}
 
 void loop() {
 
-currentState = digitalRead(buttonPin);
+  lastState1 = currentState1;
+  currentState1 = digitalRead(33);
 
-  // prüfen, ob sich der Zustand geändert hat
-  // Wenn sich der Zustand geändert hat
-  if (currentState != lastState) {
-    // wenn der Button gerade gedrückt wurde (LOW)
-    // Wenn Button gedrückt wurde (LOW)
-    if (currentState == LOW) {
-      // LED-Zustand umschalten
-      ledAn = !ledAn; // LED-Zustand umschalten
-      digitalWrite(led1, ledAn ? HIGH : LOW);
+  lastState2 = currentState2;
+  currentState2 = digitalRead(32);
 
-      // Zustände im seriellen Monitor ausgeben
-      Serial.print("Button gedrückt → LED ist jetzt: ");
-      Serial.println(ledAn ? "AN" : "AUS");
+  digitalWrite(25, HIGH);
+  digitalWrite(26, HIGH);
+
+  if (currentState1 != lastState1) {
+    if (currentState1 == LOW) {
+      ++page;
+      flash_light();
+      // Serial.println(page);
+      Serial.println("Fwd");
+      delay(50);
     }
+    
   }
 
-  if (page == 2) page = 0;
+  if (currentState2 != lastState2) {
+    if (currentState2 == LOW) {
+      --page;
+      flash_light();
+      // Serial.println(page);
+      Serial.println("Bwd");
+      delay(50);
+    }
+    
+  }
+
   if (page == 0) {
-      showWelcomePage();
-      // Serial.println(page);
+    showWelcomePage();
   } else if (page == 1) {
-      Showlightpage();
-      // Serial.println(page);
+    Showlightpage();
+  } else if (page == 2) {
+    showtempPage();
+  } else if (page == 3) {
+    page = 0;
+  } else if (page <= 0) {
+    page = 2;
   }
-
-  lastState = digitalRead(buttonPin);
 }
